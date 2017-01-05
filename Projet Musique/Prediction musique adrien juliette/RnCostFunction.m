089a482d8a7b441783660e06a958c4e80415aa08
Adrien Dufraux <adrien.dufraux@insa-rouen.fr>
2017-01-05 23:26:42 +0100

    Resolution probleme NaN

--- Projet Musique/Prediction musique adrien juliette/RnCostFunction.m
+++ Projet Musique/Prediction musique adrien juliette/RnCostFunction.m
@@ -27,6 +27,7 @@ for i=1:(L-1)
 	%On ne prend pas en compte le terme de biais. Donc on enlève la première colonne de chaque matrice theta.
 	if lambda ~= 0;
 		J = J + sum(sum((all_theta{i}(:,2:end)).^2));
+
 	end
 end
 J = (lambda/2)*J;
@@ -43,19 +44,29 @@ for k= 1:m
 	% a(1) est en fait les entrées de la première couche. z(1) ne représente rien.
 	z{1} = 0;
 	a{1} = X(k,:)';
-	for l=2:L
+	for l=2:L-1
 		z{l} = all_theta{l-1}*[1 ; a{l-1}];
 		a{l} = sigmoid(z{l});
 	end
+	%Pas de sigmoid pour la dernière couche
+	z{L} = all_theta{L-1}*[1 ; a{L-1}];
+	a{L} = z{L};
 	% a(L) contient h(x). La sortie sur la dernière couche.
 	% Le sum correxpond à la somme sur K, la taille de la dernière couche.
-	J = J - sum(y(k,:)'.*log(a{L}) + (1-y(k,:)').*log(1-a{L}));
+	%J = J - sum(y(k,:)'.*log(a{L}) + (1-y(k,:)').*log(1-a{L}));
+	%Test d'une autre fonction de cout
+	J = J + sum((a{L} - y(k,:)').^2);
+	%disp(sprintf('%f',J))
+	%if J == NaN
+	%	disp(J);
+	%end
 
 
 	% On calcule les dérivées partielles grâce à l'algorithme de backpropagation
 
 	% delta contient les erreurs de chaque neurones sur chaque couche.
 	delta = {};
+	%delta{L} = (a{L} - y(k,:)').*sigmoidGradient(z{L});
 	delta{L} = a{L} - y(k,:)';
 	all_theta_grad{L-1} = all_theta_grad{L-1} + delta{L}*[1 ; a{L-1}]';
 
@@ -66,7 +77,7 @@ for k= 1:m
 	end
 end 
 
-J = J/m;
+J = J/(2*m);
 
 % On doit diviser par m toutes les matrices de gradient et ajouter le terme de régularisation.
 % On en profite pour dérouler les matrices dans un vecteur colonne.
