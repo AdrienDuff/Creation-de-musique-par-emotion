94bd6c14a721d53bc34ba3e775b8845665f51028
Caroline Montassier <caroline.montassier@insa-rouen.fr>
2017-01-25 15:42:41 +0100

    programme principal v1

---
+++ Projet Musique/programme_principal/programme_principal.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*-coding:Utf-8 -*
+
+import csv
+import os
+
+
+#Chargement du premier vecteur musique (3s)
+data_music = csv.reader(open("debut.csv","rb"))
+
+#Création du fichier de sortie 
+data_output = csv.writer(open("sortie.csv"))
+
+
+#Initialisation des variables
+predic = []
+
+nb_sec = input('Entrer le nombre de secondes pour la séquence en sortie : ')
+
+emo = os.system("matlab predictAllClass")
+
+"""for k in range(nb_sec/3) :
+	emo = eng.predictAllClass(all_theta_1,data_music)"""
+
